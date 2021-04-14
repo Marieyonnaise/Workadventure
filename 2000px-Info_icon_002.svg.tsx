b19f963e307cb36e95cdb422c79768f2ce7f3c7b
Marieyonnaise <82506863+Marieyonnaise@users.noreply.github.com>
2021-04-14 11:44:39 +0200

    Add files via upload

---
+++ 2000px-Info_icon_002.svg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="2000px-Info_icon_002.svg" tilewidth="32" tileheight="32" tilecount="3844" columns="62">
+ <image source="../../../Pictures/2000px-Info_icon_002.svg.png" width="2000" height="2000"/>
+</tileset>
---
+++ Räume.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Räume" tilewidth="32" tileheight="32" tilecount="297" columns="33">
+ <image source="../../../Pictures/Räume.PNG" width="1078" height="294"/>
+</tileset>
